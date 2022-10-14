eccfeadbe60a3dfd2ddcb236c3034a4f6e2924fa
Kamran <311.kamran@gmail.com>
2022-10-14 23:37:11 +0400

    Add project files.

---
+++ HomeWork_LogBook/Form1.Designer.cs
@@ -0,0 +1,370 @@
+﻿namespace HomeWork_LogBook
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.txtBox_Dersin_adi = new System.Windows.Forms.TextBox();
+            this.btn_pen = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.rbt_all = new System.Windows.Forms.RadioButton();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_diamont_count = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label1.Location = new System.Drawing.Point(24, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "FBAS_4214_AZ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label2.Location = new System.Drawing.Point(327, 23);
+            this.label2.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 33);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "(Winform)";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.radioButton1.Location = new System.Drawing.Point(45, 79);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(192, 37);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "Əsas Müəllim";
+            this.radioButton1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.radioButton2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.radioButton2.Location = new System.Drawing.Point(275, 79);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(255, 37);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Əvəzləyici Müəllim";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // txtBox_Dersin_adi
+            // 
+            this.txtBox_Dersin_adi.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.txtBox_Dersin_adi.Enabled = false;
+            this.txtBox_Dersin_adi.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point);
+            this.txtBox_Dersin_adi.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.txtBox_Dersin_adi.Location = new System.Drawing.Point(737, 23);
+            this.txtBox_Dersin_adi.Multiline = true;
+            this.txtBox_Dersin_adi.Name = "txtBox_Dersin_adi";
+            this.txtBox_Dersin_adi.PlaceholderText = "Dersin adi";
+            this.txtBox_Dersin_adi.Size = new System.Drawing.Size(444, 93);
+            this.txtBox_Dersin_adi.TabIndex = 4;
+            // 
+            // btn_pen
+            // 
+            this.btn_pen.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.btn_pen.BackgroundImage = global::HomeWork_LogBook.Properties.Resources.pen_emoji;
+            this.btn_pen.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_pen.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_pen.Location = new System.Drawing.Point(690, 74);
+            this.btn_pen.Name = "btn_pen";
+            this.btn_pen.Size = new System.Drawing.Size(41, 42);
+            this.btn_pen.TabIndex = 5;
+            this.btn_pen.UseVisualStyleBackColor = false;
+            this.btn_pen.Click += new System.EventHandler(this.btn_pen_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.BackgroundImage = global::HomeWork_LogBook.Properties.Resources.save;
+            this.btn_save.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_save.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_save.Enabled = false;
+            this.btn_save.Location = new System.Drawing.Point(1199, 23);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(38, 29);
+            this.btn_save.TabIndex = 6;
+            this.btn_save.UseVisualStyleBackColor = true;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.BackgroundImage = global::HomeWork_LogBook.Properties.Resources.cancel__v2;
+            this.btn_cancel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_cancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_cancel.Enabled = false;
+            this.btn_cancel.Location = new System.Drawing.Point(1199, 86);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(38, 30);
+            this.btn_cancel.TabIndex = 7;
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btn_cancel);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btn_save);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Controls.Add(this.btn_pen);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.txtBox_Dersin_adi);
+            this.panel1.Location = new System.Drawing.Point(16, 5);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1431, 166);
+            this.panel1.TabIndex = 8;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.SystemColors.Info;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 15F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1463, 505);
+            this.tableLayoutPanel1.TabIndex = 9;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel2.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel2.Controls.Add(this.rbt_all);
+            this.panel2.Controls.Add(this.label10);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.btn_diamont_count);
+            this.panel2.Controls.Add(this.label8);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.label6);
+            this.panel2.Controls.Add(this.label7);
+            this.panel2.Location = new System.Drawing.Point(17, 180);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1428, 67);
+            this.panel2.TabIndex = 103;
+            // 
+            // rbt_all
+            // 
+            this.rbt_all.AutoSize = true;
+            this.rbt_all.BackColor = System.Drawing.Color.LimeGreen;
+            this.rbt_all.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rbt_all.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.rbt_all.Font = new System.Drawing.Font("Times New Roman", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.rbt_all.Location = new System.Drawing.Point(450, 40);
+            this.rbt_all.Name = "rbt_all";
+            this.rbt_all.Size = new System.Drawing.Size(30, 16);
+            this.rbt_all.TabIndex = 10;
+            this.rbt_all.TabStop = true;
+            this.rbt_all.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.rbt_all.UseVisualStyleBackColor = false;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label10.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label10.Location = new System.Drawing.Point(1198, 4);
+            this.label10.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(103, 26);
+            this.label10.TabIndex = 0;
+            this.label10.Text = "Comment";
+            this.label10.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label3.Location = new System.Drawing.Point(23, 9);
+            this.label3.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(145, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Student\'s Full Name";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btn_diamont_count
+            // 
+            this.btn_diamont_count.BackgroundImage = global::HomeWork_LogBook.Properties.Resources.Tamdiamond;
+            this.btn_diamont_count.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btn_diamont_count.Enabled = false;
+            this.btn_diamont_count.Location = new System.Drawing.Point(1052, 3);
+            this.btn_diamont_count.Name = "btn_diamont_count";
+            this.btn_diamont_count.Size = new System.Drawing.Size(52, 26);
+            this.btn_diamont_count.TabIndex = 101;
+            this.btn_diamont_count.UseVisualStyleBackColor = true;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label8.Location = new System.Drawing.Point(1023, 6);
+            this.label8.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(23, 26);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "5";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label4.Location = new System.Drawing.Point(193, 9);
+            this.label4.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(145, 19);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Last Seen in MyStat";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label5.Location = new System.Drawing.Point(404, 9);
+            this.label5.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Mark All Present";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label6.Location = new System.Drawing.Point(658, 7);
+            this.label6.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 22);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Test";
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.label7.Location = new System.Drawing.Point(828, 7);
+            this.label7.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 22);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "ClassWork";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(1463, 822);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Times New Roman", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private RadioButton radioButton1;
+        private RadioButton radioButton2;
+        private TextBox txtBox_Dersin_adi;
+        private Button btn_pen;
+        private Button btn_save;
+        private Button btn_cancel;
+        private Panel panel1;
+        private TableLayoutPanel tableLayoutPanel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label10;
+        private RadioButton rbt_all;
+        private Panel panel2;
+        private Button btn_diamont_count;
+    }
+}
---
+++ HomeWork_LogBook/Form1.cs
@@ -0,0 +1,43 @@
+﻿namespace HomeWork_LogBook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        #region Dersin adi
+
+
+        private void btn_pen_Click(object sender, EventArgs e)
+        {
+            txtBox_Dersin_adi.Enabled = true;
+            btn_save.Enabled = true;
+            btn_cancel.Enabled = true;
+
+            btn_cancel.Click += Btn_cancel_Click;
+            btn_save.Click += Btn_save_Click;
+        }
+
+        private void Btn_save_Click(object? sender, EventArgs e)
+        {
+            txtBox_Dersin_adi.Enabled = false;
+            btn_save.Enabled = false;
+            btn_cancel.Enabled=false;
+        }
+        private void Btn_cancel_Click(object? sender, EventArgs e)
+        {
+            txtBox_Dersin_adi.Text = string.Empty;
+            txtBox_Dersin_adi.Enabled = false;
+
+            btn_save.Enabled = false;
+            btn_cancel.Enabled = false;
+        }
+
+        #endregion
+
+
+
+    }
+}
