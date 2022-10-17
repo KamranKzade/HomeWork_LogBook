35357fa9de8d018facdbfec0ed3b09c65388cd8a
Kamran <311.kamran@gmail.com>
2022-10-17 12:01:02 +0400

    Full Hazir, Kristal Funksiyasi yoxdu

--- HomeWork_LogBook/UserControls/Uc_Student.Designer.cs
+++ HomeWork_LogBook/UserControls/Uc_Student.Designer.cs
@@ -428,6 +428,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Transparent;
             this.Controls.Add(this.label1);
             this.Controls.Add(this.tableLayoutPanel1);
             this.MinimumSize = new System.Drawing.Size(900, 0);
